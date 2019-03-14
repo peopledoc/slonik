1ddaa4f9f1444be28c5a8c72b215c0ce428fb9e6
Antoine Rozo <antoine.rozo@gmail.com>
2019-03-14 13:49:16 +0100

    Fix & complete tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,5 +6,9 @@ from slonik import Connection
 @pytest.fixture()
 def conn():
     conn_ = Connection.from_env()
-    yield conn_
-    # TODO: close the connection
+    conn_.execute('BEGIN')
+    try:
+        yield conn_
+    finally:
+        conn_.execute('ROLLBACK')
+        # TODO: close the connection
