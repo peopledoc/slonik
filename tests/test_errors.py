8f588250170904f88887efd42d27785deb5f7ac1
Antoine Rozo <antoine.rozo@gmail.com>
2019-08-23 14:54:21 +0200

    Fix tests for pytest 5

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -8,24 +8,24 @@ def test_connection_error():
     conn = Connection('postgresql://255.255.255.255/db')
     with pytest.raises(SlonikException) as e:
         conn._conn
-    assert 'Network is unreachable' in str(e)
+    assert 'Network is unreachable' in str(e.value)
 
 
 def test_query_error(conn):
     with pytest.raises(SlonikException) as e:
         list(conn.query('SELECT bar FROM foo'))
-    assert 'relation "foo" does not exist' in str(e)
+    assert 'relation "foo" does not exist' in str(e.value)
 
     with pytest.raises(SlonikException) as e:
         list(conn.query('SELECT bar MORF foo'))
-    assert 'syntax error at or near "foo"' in str(e)
+    assert 'syntax error at or near "foo"' in str(e.value)
 
 
 def test_execute_error(conn):
     with pytest.raises(SlonikException) as e:
         conn.execute('DELETE FROM foo')
-    assert 'relation "foo" does not exist' in str(e)
+    assert 'relation "foo" does not exist' in str(e.value)
 
     with pytest.raises(SlonikException) as e:
         conn.execute('DELETE MORF foo')
-    assert 'syntax error at or near "MORF"' in str(e)
+    assert 'syntax error at or near "MORF"' in str(e.value)
